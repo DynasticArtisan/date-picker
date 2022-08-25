84648f6c8fecbaf6304138778d73108e1e3b189d
Alex <webdev@artgorka.ru>
2022-08-25 10:56:20 +0300

    v0.2

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,66 +1,122 @@
 import { useCallback, useState } from "react";
 import { useCalendar } from "./hooks/useCalendar";
 
-function compareDays(day1, day2) {
-  return day1.dayNumber === day2.dayNumber && day1.monthNumber === day2.monthNumber && day1.year === day2.year
-}
-function checkInRange(day1, day2, day3){
-  if(day1.timestamp < day2.timestamp){
-    return day3.timestamp < day2.timestamp && day3.timestamp > day1.timestamp
-  }
-  return day3.timestamp > day2.timestamp && day3.timestamp < day1.timestamp
-}
-
 function Calendar(options) {
-  const { state, actions } = useCalendar(options)
-  const [open, setOpen] = useState(false)
-  const [hoverOn, setHoverOn] = useState(null)
-  
-  const openCalendar = useCallback((e)=>{
-    e.stopPropagation()
-    setOpen(true)
-    window.addEventListener('click', closeCalendar)
-  },[])
-  const closeCalendar = useCallback((e)=>{
-    e.stopPropagation()
-    if(e.target.closest('.calendar__wrapper')){
-      return null
-    }
-    setOpen(false)
-    window.removeEventListener('click', closeCalendar)
-  },[])
+  const { state, actions, helpers } = useCalendar(options);
+  const [open, setOpen] = useState(false);
 
+  const closeCalendar = useCallback((e) => {
+    e.stopPropagation();
+    if (e.target.closest(".calendar__wrapper")) {
+      return null;
+    }
+    setOpen(false);
+    window.removeEventListener("click", closeCalendar);
+  }, []);
+  const openCalendar = useCallback((e) => {
+    e.stopPropagation();
+    setOpen(true);
+    window.addEventListener("click", closeCalendar);
+  }, []);
 
   return (
     <div className="calendar">
-      <div className="calendar__output" onClick={openCalendar}>{state.date?.date?.toLocaleString(options.locale, { day: '2-digit', month: '2-digit', year: 'numeric' })}{ (state.rangeMode && state.date) ? " - " : '' }{state.selectedSecondDay?.date.toLocaleString(options.locale, { day: '2-digit', month: '2-digit', year: 'numeric' })}</div>
-      <div className={`calendar__wrapper ${open ? '--open':''}`}>
+      <div className="calendar__output" onClick={openCalendar}>
+        {!state.rangeMode &&
+          state.date &&
+          state.date.date?.toLocaleString(options.locale, options.format)}
+        {state.rangeMode &&
+          state.dateFrom &&
+          state.dateFrom.date?.toLocaleString(options.locale, options.format) +
+            " - "}
+        {state.rangeMode &&
+          state.dateTo &&
+          state.dateTo.date?.toLocaleString(options.locale, options.format)}
+      </div>
+      <div className={`calendar__wrapper ${open ? "--open" : ""}`}>
         <div className="calendar__monthes">
-          <button className="calendar__prevMonth" type="button" onClick={actions.prevMonth}>&lt;</button>
-          <div className="calendar__currentMonth">{state.selectedMonth.monthName} {state.selectedMonth.year}</div>
-          <button className="calendar__nextMonth" type="button" onClick={actions.nextMonth}>&gt;</button>
+          <button
+            className="calendar__prevMonth"
+            type="button"
+            onClick={actions.prevMonth}
+          >
+            &lt;
+          </button>
+          <div className="calendar__currentMonth">
+            {state.selectedMonth.monthName} {state.selectedMonth.year}
+          </div>
+          <button
+            className="calendar__nextMonth"
+            type="button"
+            onClick={actions.nextMonth}
+          >
+            &gt;
+          </button>
         </div>
         <div className="calendar__days">
-          { state.weekDaysNames.map(day => <div className="calendar__day" key={day.dayNumberInWeek}>{day.dayShort}</div>) }
+          {state.weekDaysNames.map((day) => (
+            <div className="calendar__day" key={day.dayNumberInWeek}>
+              {day.dayShort}
+            </div>
+          ))}
         </div>
-        <div className="calendar__dates" onMouseLeave={()=>setHoverOn(null)}>
-          { state.calendarDays.map((day, i) => <div className={`calendar__date ${state.date && compareDays(day, state.date) || state.selectedSecondDay && compareDays(day, state.selectedSecondDay) ? 'selected':''} ${state.date && state.selectedSecondDay && checkInRange(state.date, state.selectedSecondDay, day) ? '--in-range':''} ${hoverOn && checkInRange(state.date, hoverOn, day) ? '--hovered' :''}`} key={i} onClick={()=> actions.selectDay(day)} onMouseEnter={ (state.rangeMode && state.date && !state.selectedSecondDay) ? (()=>setHoverOn(day)) : null }> {day.dayNumber}</div> ) }
+        <div
+          className="calendar__dates"
+          onMouseLeave={() => actions.hoverOnDate(null)}
+        >
+          {state.calendarDays.map((day, i) => (
+            <div
+              className={`calendar__date ${
+                helpers.isSelected(day) ? "selected" : ""
+              } 
+              ${helpers.isInRange(day) ? "--in-range" : ""} 
+              ${helpers.isInHoverRange(day) ? "--hovered" : ""}`}
+              key={i}
+              onClick={() => actions.selectDay(day)}
+              onMouseEnter={() => actions.hoverOnDate(day)}
+            >
+              {day.dayNumber}
+            </div>
+          ))}
         </div>
-        <div className={`calendar__range-mode ${state.rangeMode ? '--selected':''}`} onClick={actions.toggleRangeMode}>Выбрать период</div>
+        <div
+          className={`calendar__range-mode ${
+            state.rangeMode ? "--selected" : ""
+          }`}
+          onClick={actions.toggleRangeMode}
+        >
+          Выбрать период
+        </div>
       </div>
-      {
-        state.rangeMode ? (
-          <>
-            <input name="date_from" type="text" value={state.date?.timestamp} hidden/>
-            <input name="date-to" type="text" value={state.selectedSecondDay?.timestamp} hidden/>
-          </>
-        )
-          : (
-              <input name="date" type="text" value={state.date?.timestamp} hidden/>
-          )
-         
-      }
-      <input name="rangeMode" type="checkbox" checked={state.rangeMode} hidden/>
+      {state.rangeMode ? (
+        <>
+          <input
+            name="dateFrom"
+            type="text"
+            value={state.date?.timestamp || ""}
+            hidden
+          />
+          <input
+            name="dateTo"
+            type="text"
+            value={state.selectedSecondDay?.timestamp || ""}
+            hidden
+          />
+        </>
+      ) : (
+        <input
+          name="date"
+          type="text"
+          value={state.date?.timestamp || ""}
+          hidden
+        />
+      )}
+      <input
+        name="rangeMode"
+        type="checkbox"
+        checked={state.rangeMode || false}
+        hidden
+      />
     </div>
   );
 }
--- src/hooks/useCalendar.js
+++ src/hooks/useCalendar.js
@@ -2,34 +2,46 @@ import { useCallback, useMemo, useReducer, useState } from "react"
 import { createMonth, getWeekDaysNames } from "../utils"
 
 
-export const useCalendar = (options) => {
-    const locale = 'default'
-    const [date, setDate] = useState(new Date(options.date))
-    const [dateFrom, setDateFrom] = useState(new Date(options.dateFrom))
-    const [dateTo, setDateTo] = useState(new Date(options.dateTo))
+function isEqualDate(date1, date2){
+    return (
+        date1?.dayNumber === date2?.dayNumber &&
+        date1?.monthNumber === date2?.monthNumber &&
+        date1?.year === date2?.year
+      );
+}
 
+export const useCalendar = (options) => {
+    const [hoverDate, hoverOnDate] = useState(null)
+    const [date, setDate] = useReducer(( _ , date)=>{
+        options.setDate(date)
+        return date
+    }, new Date(options.date))
+    const [dateFrom, setDateFrom] = useReducer(( _ , date)=>{
+        options.setDateFrom(date)
+        return date
+    }, new Date(options.dateFrom))
+    const [dateTo, setDateTo] = useReducer(( _ , date)=>{
+        options.setDateTo(date)
+        return date
+    }, new Date(options.dateTo))
     const [ rangeMode, toggleRangeMode ] = useReducer(prev => {
         setDate(null)
-        options.date = null
         setDateFrom(null)
-        options.dateFrom = null
         setDateTo(null)
-        options.dateTo = null
+        options.setRangeMode(!prev)
         return !prev
     }, options.rangeMode)
-
     const [ selectedMonth, setSelectedMonth ] = useState(createMonth(options.date || options.dateTo || options.dateFrom, options.locale))
 
     const weekDaysNames = useMemo(()=> getWeekDaysNames(options.locale), [])
-
     const calendarDays = useMemo(()=>{
         const selectedMonthDays = selectedMonth.createMonthDays()
 
         const prevDaysCount = selectedMonthDays[0].dayNumberInWeek
-        const prevMonthDays = prevDaysCount ? createMonth(new Date(selectedMonth.monthIndex > 0 ? selectedMonth.year : selectedMonth.year - 1, selectedMonth.monthIndex > 0 ? selectedMonth.monthIndex - 1 : 11), locale).createMonthDays().slice(-1 * prevDaysCount) : []
+        const prevMonthDays = prevDaysCount ? createMonth(new Date(selectedMonth.monthIndex > 0 ? selectedMonth.year : selectedMonth.year - 1, selectedMonth.monthIndex > 0 ? selectedMonth.monthIndex - 1 : 11), options.locale).createMonthDays().slice(-1 * prevDaysCount) : []
 
         const nextDaysCount = 6 - selectedMonthDays[selectedMonthDays.length - 1].dayNumberInWeek
-        const nextMonthDays = nextDaysCount ? createMonth(new Date(selectedMonth.monthIndex < 11 ? selectedMonth.year : selectedMonth.year + 1, selectedMonth.monthIndex < 11 ? selectedMonth.monthIndex + 1 : 0), locale).createMonthDays().slice(0, nextDaysCount) : []
+        const nextMonthDays = nextDaysCount ? createMonth(new Date(selectedMonth.monthIndex < 11 ? selectedMonth.year : selectedMonth.year + 1, selectedMonth.monthIndex < 11 ? selectedMonth.monthIndex + 1 : 0), options.locale).createMonthDays().slice(0, nextDaysCount) : []
 
 
         return [ ...prevMonthDays, ...selectedMonthDays, ...nextMonthDays ]
@@ -38,50 +50,68 @@ export const useCalendar = (options) => {
     const prevMonth = useCallback(()=>{
         setSelectedMonth(current => {
             if(current.monthIndex > 0){
-                return createMonth(new Date(current.year, current.monthIndex - 1), locale)
+                return createMonth(new Date(current.year, current.monthIndex - 1), options.locale)
             }
-            return createMonth(new Date(current.year - 1, 11), locale)  
+            return createMonth(new Date(current.year - 1, 11), options.locale)  
         })
-    }, [locale])
-
+    }, [])
     const nextMonth = useCallback(()=>{
         setSelectedMonth(current => {
             if(current.monthIndex < 11){
-                return createMonth(new Date(current.year, current.monthIndex + 1), locale)
+                return createMonth(new Date(current.year, current.monthIndex + 1), options.locale)
             }
-            return createMonth(new Date(current.year + 1, 0), locale)  
+            return createMonth(new Date(current.year + 1, 0), options.locale)  
         })
-    }, [locale])
-
+    }, [])
     const selectDay = (day)=>{
         if(!rangeMode){
-            options.setDate(day.date)
             return setDate(day)
         }
         if(!dateFrom){
-            options.setDateFrom(day.date)
             return setDateFrom(day)
         }
         if(!dateTo){
             if(dateFrom.timestamp > day.timestamp){
-                options.setDateTo(dateFrom.date)
                 setDateTo(dateFrom)
-                options.setDateFrom(day.date)
                 return setDateFrom(day)
             }
-            options.setDateTo(day.date)
             return setDateTo(day)
         }
-        options.setDateFrom(day.date)
-        setDateFrom(day)
+        setDateTo(null)
+        return setDateFrom(day)
     }
+
+
+    const isSelected = useCallback((day) => {
+        return (
+            isEqualDate(day, date) || isEqualDate(day, dateFrom) || isEqualDate(day, dateTo)
+          );
+    }, [date, dateFrom, dateTo])
+    const isInRange = useCallback((day)=>{
+        if(!dateFrom || !dateTo){
+            return false
+        }
+        return day.timestamp > dateFrom.timestamp && day.timestamp < dateTo.timestamp
+    }, [dateFrom, dateTo])
+    const isInHoverRange = useCallback((day)=>{
+        if(!dateFrom || !hoverDate || dateTo){
+            return false
+        }
+        if(hoverDate.timestamp > dateFrom.timestamp){
+            return day.timestamp > dateFrom.timestamp && day.timestamp < hoverDate.timestamp
+        }
+        return day.timestamp < dateFrom.timestamp && day.timestamp > hoverDate.timestamp
+    }, [dateFrom, hoverDate])
 
     return {
         state: {
             weekDaysNames, calendarDays, selectedMonth, date, dateFrom, dateTo, rangeMode
         },
         actions: {
-            nextMonth, prevMonth, toggleRangeMode, selectDay
+            nextMonth, prevMonth, toggleRangeMode, selectDay, hoverOnDate
+        },
+        helpers: {
+            isSelected, isInRange, isInHoverRange
         }
     }
     
